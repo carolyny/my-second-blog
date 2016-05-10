8b48294db3cbf06441aabbc7674c9c529b93bd70
Carolyn Yates <2carolynyates@gmail.com>
2016-05-10 15:48:27 +0100

    Menu bar links working

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+
+    url(r'^$',views.about, name='about'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^post/$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
 ]
